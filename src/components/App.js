a9537361d42b0ebddfa99f57613aa693684defa1
Michael DoHo <michaeldoho@hotmail.com>
2019-04-02 14:00:15 -0400

    added SlideBox component and App/SlideBox sass styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import '../styles/App.scss';
+import SlideBox from './SlideBox.js';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        test
+        <SlideBox />
       </div>
     );
   }
---
+++ src/components/SlideBox.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/SlideBox.scss';
+
+export default class SlideBox extends Component {
+  render() {
+    return (
+      <div className='slide-container'>
+    
+      </div>
+    )
+  }
+}
