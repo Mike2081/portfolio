a179d2e2b7cd7b41ecd54f5eb1180d0324be7e29
Michael DoHo <michaeldoho@hotmail.com>
2019-04-03 11:20:45 -0400

    Added font and bem styling to slidebox component

--- src/components/SlideBox.js
+++ src/components/SlideBox.js
@@ -4,8 +4,16 @@ import '../styles/SlideBox.scss';
 export default class SlideBox extends Component {
   render() {
     return (
-      <div className='slide-container'>
-    
+      <div className='sContainer'>
+        <div className='sContainer__home1'>
+        Hi, I'm
+        </div>
+        <div className='sContainer__home2'>
+        Michael DoHo
+        </div>
+        <div className='sContainer__home3'>
+          I'm a front-end web developer.
+        </div>  
       </div>
     )
   }
