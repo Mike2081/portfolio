c8e7b243b04331c90961ded10410dd567d78eb06
Michael DoHo <michaeldoho@hotmail.com>
2019-04-06 14:28:56 -0400

    finished the slide animation function for SlideBox.js component

--- src/components/SlideBox.js
+++ src/components/SlideBox.js
@@ -2,27 +2,34 @@ import React, { Component } from 'react';
 import '../styles/SlideBox.scss';
 
 export default class SlideBox extends Component {
-  componentDidMount(){
-  let headerOnScroll = function() {
-   let header = document.getElementsByClassName("sContainer");
-    if(window.pageYOffset === 0) {
-      header.classList.add('view');
-      alert('top');
-  }else {
-    header.classList.remove('view');
-  }
-  };
+  constructor() {
+    super();
+    this.state = {class: "sContainer"};
+};
+componentDidMount(){
+  window.addEventListener('scroll', (event) => {
+     if(window.pageYOffset < 100) {
+      this.setState({
+        class: 'sContainer-view'
+     })
+     }else{
+      this.setState({
+        class: 'sContainer'
+  })
+    }
+  });
 }
+
   render() {
     return (
-      <div className='sContainer'>
-        <div className='sContainer__home1'>
+      <div className = {this.state.class}>
+        <div className='home1'>
         Hi, I'm
         </div>
-        <div className='sContainer__home2'>
+        <div className='home2'>
         Michael DoHo
         </div>
-        <div className='sContainer__home3'>
+        <div className='home3'>
           I'm a front-end web developer.
         </div>  
       </div>
