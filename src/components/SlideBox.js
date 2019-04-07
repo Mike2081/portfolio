70c01025540836b760a62ae9d499857d24529412
Michael DoHo <michaeldoho@hotmail.com>
2019-04-07 13:06:52 -0400

    switched to new new css animation for SlideBox.js component

--- src/components/SlideBox.js
+++ src/components/SlideBox.js
@@ -1,38 +1,17 @@
 import React, { Component } from 'react';
 import '../styles/SlideBox.scss';
-
 export default class SlideBox extends Component {
-  constructor() {
-    super();
-    this.state = {class: "sContainer"};
-};
-componentDidMount(){
-  window.addEventListener('scroll', (event) => {
-     if(window.pageYOffset < 100) {
-      this.setState({
-        class: 'sContainer-view'
-     })
-     }else{
-      this.setState({
-        class: 'sContainer'
-  })
-    }
-  });
-}
-
   render() {
     return (
-      <div className = {this.state.class}>
-        <div className='home1'>
-        Hi, I'm
-        </div>
-        <div className='home2'>
-        Michael DoHo
-        </div>
-        <div className='home3'>
-          I'm a front-end web developer.
-        </div>  
-      </div>
+      <div className='container'>
+  Hi, I'm 
+  <div className='flip'>
+    <div><div>Michael DoHo</div></div>
+    <div><div>Michael DoHo</div></div>
+    <div><div>Michael DoHo</div></div>
+  </div>
+  Front End Developer
+</div>
     )
   }
 }
