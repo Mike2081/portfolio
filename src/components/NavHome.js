5d734413044d3e376fcd577a362e69bd742fbccb
Michael DoHo <michaeldoho@hotmail.com>
2019-05-01 12:29:39 -0400

    started working on scolling into view function

--- src/components/NavHome.js
+++ src/components/NavHome.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import '../styles/NavHome.scss';
 
 export default class NavHome extends Component {
+  scrollStep() {
+    if (window.pageYOffset === 0) {
+        clearInterval(this.state.intervalId);
+    }
+    window.scroll(0, window.pageYOffset);
+  }
   render() {
     return (
       <div className='back'>
